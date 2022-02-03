9661f941161e261b047ecf00f9f5a1cd73310eeb
Golamrabbani-ux <golamrabbani227417@gmail.com>
2022-02-03 18:00:40 +0600

    url update

--- src/helper/axios-utils.js
+++ src/helper/axios-utils.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const client = axios.create({ baseURL: process.env.REACT_APP_API_URL });
+const client = axios.create({ baseURL: process.env.REACT_APP_API_URL_MAIN });
 
 export const request = async ({ ...options }) => {
   const res = await client(options);
